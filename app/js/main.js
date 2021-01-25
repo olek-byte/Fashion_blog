d1bb2612d013122212f923919325c9f4d53ca6f9
olek-byte <olekhyzhniak@gmail.com>
2021-01-25 21:38:43 +0200

    fixed  Fashion_blog page

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,59 @@
 $(function(){
 
+
+     $('.slider-bottom__list').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,      
+      responsive: [
+        {
+          breakpoint: 1750,
+          settings: {
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            arrows: false,
+          }
+        },
+
+        {
+          breakpoint: 1199,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            arrows: false,
+          }
+        },
+
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            arrows: false,
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            arrows: false
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            arrows: false,
+          }
+        }
+      ]
+  });
+
+
+
 });
